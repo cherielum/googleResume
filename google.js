f127420d2af0cbe4a4789c91ca547577bd38f1dc
Cherie Lum <charismatic.cherie@gmail.com>
2017-10-25 13:34:06 -0400

    adding first draft

---
+++ google.js
@@ -0,0 +1,16 @@
+function pink() {
+  document.body.style.backgroundColor="#ff68d1"; 
+
+}
+
+function blue() {
+	document.body.style.backgroundColor="#00ddff";
+}
+
+function green() {
+	document.body.style.backgroundColor="#50fc78";
+}
+
+function white() {
+	document.body.style.backgroundColor= "white";
+}
